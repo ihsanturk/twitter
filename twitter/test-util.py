de65787f363b340cacbd039a7f3f9853a12dd20b
ihsan <ihsanl@pm.me>
2020-11-04 13:14:51 +0300

    add: unit tests, fix: get_lastpos()

--- twitter/test-util.py
+++ twitter/test-util.py
@@ -3,28 +3,28 @@ from twitter.util import *
 from pymongo import MongoClient
 from twitter.mongo_credentials import port, username, password, host
 
-# NOTE: NOT TESTED: THIS FILE
+unittest.TestLoader.sortTestMethodsUsing = None
 
 class TestDbFunctions(unittest.TestCase):
 
-	def setUp(self):
-		self.c = "test-company"
-		self.d = "2020-10-30 12:24:55"
-		self.dbclient = MongoClient(f'mongodb://{host}:{port}')
-		self.db = dbclient['twitter']
+	def setUp(s):
+		s.c = "test-company"
+		s.dbname = 'test-twitter'
+		s.dbclient = MongoClient(f'mongodb://{host}:{port}')
+		s.db = s.dbclient[s.dbname]
 
-	def test_set_lastpos(self, )
-		pass
+	def test_init_lastpos(s):
+		date = '2000-01-01 00:00:00'
+		date = '2020-11-02 15:00:00' #TODO#p: dd
+		s.assertEqual(init_lastpos(s.db.tweets, s.c), date)
 
-	def test_init_lastpos(self, ):
-		pass
+	def test_set_get_lastpos(s):
+		set_lastpos(s.db['tweets'], s.c, "2020-11-02 20:00:57")
+		s.assertEqual(get_lastpos(s.db['tweets'], s.c), "2020-11-02 20:00:57")
 
-	def test_get_lastpos(self, ):
-		pass
-
-	def tearDown(self):
-		db.
+	def tearDown(s):
+		s.dbclient.drop_database('test-twitter')
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 	unittest.main()
--- twitter/util.py
+++ twitter/util.py
@@ -1,9 +1,9 @@
 import sys
 import logging
 import pymongo
+import twitter.error
 
 debug = logging.debug
-# yellerr = lambda msg: sys.stderr.write(sys.argv[0]+': '+str(msg))
 
 def readfile(fl):
 	debug(f'reading file: {fl}')
@@ -16,37 +16,42 @@ def readfile(fl):
 		sys.exit(2)
 
 # mongo ---------------------------------------------------------------------
-def init_lastpos(coll, query):
+def init_lastpos(mongocollection, query):
 	debug(f'initializing last pos value for {query}')
 	date = '2000-01-01 00:00:00'
-	date = '2020-10-27 21:00:00' #TODO#p: dd
-	return set_lastpos(coll, query, date)
+	date = '2020-11-02 15:00:00' #TODO#p: dd
+	return set_lastpos(mongocollection, query, date)
 
-def set_lastpos(coll, query, date):
+def set_lastpos(mongocollection, query, date):
 	#TODO#0: store date as python date object.
 	debug(f'setting last pos value for {query} to {date}')
-	try:
-		lastpos = coll.insert_one({ "_id": query, 'lastpos': date }, upsert=True)
+	d = { "_id": query, "lastpos": date }
+	# try:
+	if mongocollection.replace_one(d, d, True).raw_result['ok'] == 1.0:
 		return date
-	except pymongo.errors.ServerSelectionTimeoutError:
-		logging.critical(f'could not connected to mongodb')
-		sys.exit(2)
-	# except pymongo.errors.DuplicateError: #TODO:e Add the correct error
-		# return -1
+	else:
+		raise Exception() # TODO#2: dd
+		# raise CannotSetLastPos(mongocollection, query, date) TODO#2: add this to twitter/error.py
+	# except pymongo.errors.ServerSelectionTimeoutError:
+	# 	logging.critical(f'could not connected to mongodb')
+		# sys.exit(2)
 
-def get_lastpos(coll, query):
+def get_lastpos(mongocollection, query):
 	#TODO#0: convert from date object to string.
 	debug(f'getting last pos value for {query}')
 	try:
-		date = coll.find_one({"_id": query}, { 'lastpos': 1, '_id': 0 })
-		if not date: debug(f"can't get last position value for {query}")
-		return None
+		date = mongocollection.find_one({"_id": query}, {'lastpos': 1, '_id':
+			0})['lastpos']
+		if not date: raise twitter.error.NoLastPositionData(query)
 	except pymongo.errors.ServerSelectionTimeoutError:
 		logging.critical(f'could not connected to mongodb')
 		sys.exit(2)
+	return date
 
-def mongo_save(coll, data):
-	coll.insert_one(data)
+def mongo_save(mongocollection, document):
+	print(document['datetime'])
+	# mongocollection.(document)
+
 
 # # redis -------------------------------------------------------------------
 # def set_lastpos(redis_client, query, date):
