39df5e108341a8d81a152b6672a4be345b0f1737
ihsan <ihsanl@pm.me>
2020-11-11 20:22:16 +0300

    tidy up

--- twitter/cli.py
+++ twitter/cli.py
@@ -30,18 +30,6 @@ logging.basicConfig(level=logging.WARN) #DEBUG)
 dbclient = MongoClient(f'mongodb://{host}:{port}')
 db = dbclient['twitter']
 
-# overwrite json method to store tweets in db.
-module = sys.modules["twint.storage.write"]
-def Json(obj, config):
-	tweet = obj.__dict__ # see: readme.md twint tweets section to see fields.
-	tweet['_id'] = tweet.pop('id'); # mongodb compatible id
-	tweet['datetime'] = datetime.strptime(tweet.pop('datetime'), '%Y-%m-%d %H:%M:%S %Z') # convert string date to date object
-	tweet['captured_datetime'] = datetime.now();
-	delta = tweet['captured_datetime'] - tweet['datetime']
-	tweet['captured_delay_sec'] = delta.total_seconds()
-	mongo_save(db, tweet, config.Search)
-module.Json = Json
-
 now = lambda: str(datetime.now()).split('.')[0]
 
 def main():
@@ -56,9 +44,12 @@ def main():
 	c.Output = "tweets.json" # just to satisfy the twint, not acutal write.
 	c.Hide_output = True
 	c.Lang = arg['--lang']
-	# c.Limit = 1; #TODO#p: dd
 
-	db.tweets.create_index([("tweet", pymongo.TEXT)], background=True) # respects if exists
+	db.tweets.create_index([("tweet", pymongo.TEXT)], # respects if exists
+		background=True)
+
+	# TODO: Create list of twint.Config for every query and use threadpool.
+	# config_list = [ c for c in queries ]
 
 	# action
 	while True:
@@ -72,16 +63,28 @@ def main():
 				twint.run.Search(c)
 			except asyncio.exceptions.TimeoutError:
 				continue
-			try: #TODO#0: check if len(twint.output.tweets_list) > 0:
+			try: #TODO#p: check if len(twint.output.tweets_list) > 0:
 				lt = twint.output.tweets_list[0] # latest tweet
 				print(f'\nlt: {lt.tweet}\n')
 				set_lastpos(db.info, query, lt.datetime)
-				# time.sleep(10) #TODO#p: delete
 			except IndexError:
 				sys.stderr.write(f'no tweets found: {query}')
 				set_lastpos(db.info, query, now())
 
 			twint.output.clean_lists()
+
+# overwrite json method to store tweets in db.
+module = sys.modules["twint.storage.write"]
+def Json(obj, config):
+	tweet = obj.__dict__ # see: readme.md twint tweets section to see fields.
+	tweet['_id'] = tweet.pop('id'); # mongodb compatible id
+	tweet['datetime'] = datetime.strptime(tweet.pop('datetime'),
+		'%Y-%m-%d %H:%M:%S %Z') # convert string date to date object
+	tweet['captured_datetime'] = datetime.now();
+	delta = tweet['captured_datetime'] - tweet['datetime']
+	tweet['captured_delay_sec'] = delta.total_seconds()
+	mongo_save(db, tweet, config.Search)
+module.Json = Json
 
 import signal
 def signal_handler(sig, frame): # clean up code
