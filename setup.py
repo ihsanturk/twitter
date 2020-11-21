ede8fbd006ec0ebe328de6571df54e5416a0a4ca
ihsan <ihsanl@pm.me>
2020-11-21 22:24:51 +0300

    add: missing dependency: pymongo

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
 	version='0.0.1',
 	packages=find_packages(),
 	author_email='ihsanl@pm.me',
-	install_requires=['twint', 'docopt'],
 	description='Twitter scraper, streamer',
 	url='https://github.com/ihsanturk/twitter',
+	install_requires=['pymongo', 'twint', 'docopt'],
 	entry_points={'console_scripts': ['twitter = twitter.cli:main']},
 
 )
