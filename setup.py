e3e2528e2feca9986ccec6fa00dd6eafe4ad47bc
ihsan <ihsanl@pm.me>
2020-11-22 15:14:14 +0300

    specify the twint versiona in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	author_email='ihsanl@pm.me',
 	description='Twitter scraper, streamer',
 	url='https://github.com/ihsanturk/twitter',
-	install_requires=['pymongo', 'twint', 'docopt'],
 	entry_points={'console_scripts': ['twitter = twitter.cli:main']},
+	install_requires=['pymongo', 'twint @ git+git://github.com/twintproject/twint@ae5e7e1189be1cf319bbd55b921aca6bfb899f8c', 'docopt'],
 
 )
