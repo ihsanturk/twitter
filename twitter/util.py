ef2ef49b79b19c96a2019033a3e2005cd229b91e
ihsan <ihsanl@pm.me>
2020-11-30 14:13:58 +0300

    simplify util functions, tidy up

--- twitter/util.py
+++ twitter/util.py
@@ -6,14 +6,19 @@ from datetime import datetime, timezone
 from twitter.color import Colors as color
 # from confusables import normalize
 
-debug = logging.debug
-err   = lambda m: sys.stderr.write(str(m)+'\n')
-info  = lambda m: logging.info(f'{color.BLUE}{m}{color.END}')
-warn  = lambda m: logging.warn(f'{color.YELLOW}{m}{color.END}')
-wo_mentions = lambda t: " ".join(filter(lambda x: x[0]!='@', t.split()))
-dateparse = lambda d: datetime.strptime(d, '%Y-%m-%d %H:%M:%S %z')
-now = lambda: datetime.now(timezone.utc)
-dateformat = '%Y-%m-%d %H:%M:%S'
+debug        = logging.debug
+dateformat   = '%Y-%m-%d %H:%M:%S'
+now          = lambda: datetime.now(timezone.utc)
+language_is  = lambda l, d: (d == l or d == 'und')
+err          = lambda m: sys.stderr.write(str(m)+'\n')
+init_lastpos = lambda mc, q: set_lastpos(mc, q, now())
+includes     = lambda x, y: x.lower() in wo_mentions(y.lower())
+info         = lambda m: logging.info(f'{color.BLUE}{m}{color.END}')
+dateparse    = lambda d: datetime.strptime(d, '%Y-%m-%d %H:%M:%S %z')
+warn         = lambda m: logging.warn(f'{color.YELLOW}{m}{color.END}')
+wo_mentions  = lambda t: " ".join(filter(lambda x: x[0]!='@', t.split()))
+filters_pass = lambda q,d,l: language_is(l,d['lang']) and includes(q,d['tweet'])
+
 
 def readfile(fl):
 	debug(f'reading file: {fl}')
@@ -25,35 +30,33 @@ def readfile(fl):
 		sys.stderr.write(f'no such file or directory: {fl}\n')
 		sys.exit(2)
 
-def lastposf(mongocollection, query):
-	try: return get_lastpos(mongocollection, query)
+def lastposf(mc, query):
+	try:
+		return get_lastpos(mc, query)
 	except twitter.error.NoLastPositionData:
-		return init_lastpos(mongocollection, query)
+		return init_lastpos(mc, query)
 
-def init_lastpos(mongocollection, query):
-	debug(f'initializing last pos value for {query}')
-	return set_lastpos(mongocollection, query, now())
-
-def set_lastpos(mongocollection, query, date):
+def set_lastpos(mc, query, date):
 	debug(f'setting last pos value for {query} to {date}')
 	a = { "_id": query }
 	b = { "_id": query, "lastpos": date }
 	# try:
-	server_result = mongocollection.replace_one(a, b, True)
+	server_result = mc.replace_one(a, b, True)
 	debug(f'replace_one() server result: {server_result.raw_result}')
 	if server_result.raw_result['ok'] == 1.0: return date
 	else:
 		raise Exception() # TODO#2: dd
-		# raise CannotSetLastPos(mongocollection, query, date) TODO#2: add this to twitter/error.py
+		# raise CannotSetLastPos(mc, query, date) TODO#2: add this to twitter/error.py
 	# except pymongo.errors.ServerSelectionTimeoutError:
 	# 	logging.critical(f'could not connected to mongodb')
 		# sys.exit(2)
 
-def get_lastpos(mongocollection, query):
+def get_lastpos(mc, query):
 	debug(f'getting last pos value for {query}')
 	try:
-		result = mongocollection.find_one({"_id":query}, {'lastpos': 1, '_id': 0})
-		if not result: raise twitter.error.NoLastPositionData(query)
+		result = mc.find_one({"_id":query}, {'lastpos': 1, '_id': 0})
+		if not result:
+			raise twitter.error.NoLastPositionData(query)
 	except pymongo.errors.ServerSelectionTimeoutError:
 		logging.critical(f'could not connected to mongodb')
 		sys.exit(2)
@@ -63,24 +66,13 @@ def get_lastpos(mongocollection, query):
 
 def mongo_save(db, document, config):
 	debug(f'saving: {document}')
-	if not filters_pass(config.Search, document, config.Lang): return
-	try: db.tweets.insert_one(document)
-	except pymongo.errors.DuplicateKeyError: return
+	if not filters_pass(config.Search, document, config.Lang):
+		return
+	try:
+		db.tweets.insert_one(document)
+	except pymongo.errors.DuplicateKeyError:
+		return
 	else: # not a duplicate
 		print(f'{document["capture_delay_sec"]:4.4f}',
 			config.Search, document["tweet"], sep='\t')
-
-def filters_pass(q, d, lang):
-	language_is(lang, d['lang']) and includes(q, d['tweet'])
-
-language_is = lambda l, d: d == l or d == 'und' # NOTE: Why the fuck twint
-#                                                  language detecion is not
-#                                                  working? So I have to write
-#                                                  an extra filter?!!!
-
-includes = lambda x, y: x.lower() in wo_mentions(y.lower())
-	# # FIXME: normalize('YEŞİL') -> ['#yefil', '#yefll', '#yesil', '#yesll']
-	# if x.lower() in y.lower(): return True
-	# # elif x.lower() in normalize(y, prioritize_alpha=True)[0]: return True
-	# else: return False
 
