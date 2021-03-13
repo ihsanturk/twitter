f7184c0af6685db3dfbd2fafc94c70db47ab1304
ihsan <ihsanl@pm.me>
2021-03-14 00:31:35 +0300

    fix typo, add guesttoken function

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='twitter',
     version="2.0.1-alpha",
     py_modules=['twitter'],
+    packages = ['twitter'],
     entry_points={
         'console_scripts': [
-            'twitter = cli:main',
+            'twitter = twitter.cli:main',
         ],
     }
 )
