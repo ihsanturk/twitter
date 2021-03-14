e3ed991d8ae7522bd2a5661c75bae54f2cf819a9
ihsan <ihsanl@pm.me>
2021-03-15 00:11:53 +0300

    add: forgotten return statement

--- twitter/user.py
+++ twitter/user.py
@@ -7,6 +7,8 @@ headers = { 'authorization': bearer_token, 'x-guest-token': get_guest_token() }
 def get_user_last_tweets(user=None):
     if user is not None:
         response = get(url_user_screen + user, headers=headers)
+        if response.ok:
+            return response.text
     else:
         raise(Exception(
             'no username specified for function: get_user_last_tweets()'))
