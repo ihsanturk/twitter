16cdb9d412adfa7e1d4989b25e71a6664a94bf8f
ihsan <ihsanl@pm.me>
2021-03-15 00:14:08 +0300

    up: func name get_user_last_tweets -> profile

--- twitter/user.py
+++ twitter/user.py
@@ -4,11 +4,11 @@ from twitter.constant import bearer_token, url_user_screen
 
 headers = { 'authorization': bearer_token, 'x-guest-token': get_guest_token() }
 
-def get_user_last_tweets(user=None):
+def profile(user=None):
     if user is not None:
         response = get(url_user_screen + user, headers=headers)
         if response.ok:
-            return response.text
+            return response.json
     else:
         raise(Exception(
             'no username specified for function: get_user_last_tweets()'))
