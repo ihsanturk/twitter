75d2bae45a15b6a4683743df96163c38d02ac150
ihsan <ihsanl@pm.me>
2021-03-14 23:47:50 +0300

    rm: unnecessary urls

--- twitter/util.py
+++ twitter/util.py
@@ -2,9 +2,6 @@ from requests import get
 import re
 
 url_base     = 'https://twitter.com'
-url_mobile   = 'https://mobile.twitter.com'
-url_activate = 'https://api.twitter.com/1.1/guest/activate.json'
-
 bearer_token = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA'
 useragent    = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'
 
