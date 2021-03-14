985531e0400256e75aba1420afb63393dd8b5205
ihsan <ihsanl@pm.me>
2021-03-14 23:45:45 +0300

    tidy up

--- twitter/util.py
+++ twitter/util.py
@@ -9,8 +9,7 @@ bearer_token = 'Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4pu
 useragent    = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'
 
 def getguesttoken():
-    response = get(url_base,
-              headers={'User-Agent': useragent})
+    response = get(url_base, headers={'User-Agent': useragent})
     if response.ok:
         match = re.search(r'\("gt=(\d+);', response.text)
         if match:
